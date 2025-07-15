e5f6ac930b50e91b9b8afb1cc1a8de0b77bf50fd
Carbon-dev-code <andrianfandresenaa@gmail.com>
2025-07-15 13:00:50 +0300

    Modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import { useEffect } from 'react'; // Ajoutez cette importation
-import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonContent,
-  IonHeader,
-  IonMenu,
-  IonMenuToggle,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonPage,
-  IonRouterOutlet,
-  IonSplitPane,
-  IonTitle,
-  IonToolbar,
-  setupIonicReact,
-  IonIcon
-} from '@ionic/react';
+// App.tsx
+import { IonApp, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { documentOutline, informationOutline, mapOutline } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
-import Accueil from './pages/accueil/Accueil';
-import Login from './pages/login/Login';
+import { useEffect } from 'react';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import MainRouter from './MainRouter';
 
 import '@ionic/react/css/core.css';
 import '@ionic/react/css/normalize.css';
@@ -36,12 +16,10 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 import './theme/variables.css';
-import { StatusBar, Style } from '@capacitor/status-bar';
 
 setupIonicReact();
 
 const App: React.FC = () => {
-  // Ajoutez cet effet pour configurer la barre de statut
   useEffect(() => {
     const configureStatusBar = async () => {
       try {
@@ -52,81 +30,15 @@ const App: React.FC = () => {
         console.warn('StatusBar not available in browser', error);
       }
     };
-
     configureStatusBar();
   }, []);
 
   return (
     <IonApp>
       <IonReactRouter>
-        <IonSplitPane contentId="main-content">
-          
-          {/* === MENU LATERAL === */}
-          <IonMenu contentId="main-content" type="overlay">
-            <IonHeader>
-              <IonToolbar>
-                <IonTitle>TopoManager</IonTitle>
-              </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-              <IonList>
-                <IonMenuToggle autoHide={false}>
-                  <IonItem routerLink="/tab1" routerDirection="none">
-                    <IonIcon icon={documentOutline} slot="start" />
-                    <IonLabel>Formulaire</IonLabel>
-                  </IonItem>
-                </IonMenuToggle>
-                <IonMenuToggle autoHide={false}>
-                  <IonItem routerLink="/tab2" routerDirection="none">
-                    <IonIcon icon={mapOutline} slot="start" />
-                    <IonLabel>Carte</IonLabel>
-                  </IonItem>
-                </IonMenuToggle>
-                <IonMenuToggle autoHide={false}>
-                  <IonItem routerLink="/tab3" routerDirection="none">
-                    <IonIcon icon={informationOutline} slot="start" />
-                    <IonLabel>Syncronisation</IonLabel>
-                  </IonItem>
-                </IonMenuToggle>
-              </IonList>
-            </IonContent>
-          </IonMenu>
-
-          {/* === CONTENU PRINCIPAL === */}
-          <IonPage id="main-content">
-            {/* Ajoutez cette div wrapper avec safe-area */}
-            <div style={{
-              paddingTop: 'var(--ion-safe-area-top, 0)',
-              paddingBottom: 'var(--ion-safe-area-bottom, 0)',
-              height: '100%'
-            }}>
-              <IonRouterOutlet>
-                <Route exact path="/accueil">
-                  <Accueil />
-                </Route>
-                <Route exact path="/login">
-                  <Login />
-                </Route>
-                <Route exact path="/tab1">
-                  <Tab1 />
-                </Route>
-                <Route exact path="/tab2">
-                  <Tab2 />
-                </Route>
-                <Route path="/tab3">
-                  <Tab3 />
-                </Route>
-                <Route exact path="/">
-                  <Redirect to="/accueil" />
-                </Route>
-              </IonRouterOutlet>
-            </div>
-          </IonPage>
-
-        </IonSplitPane>
+        <MainRouter />
       </IonReactRouter>
     </IonApp>
   );
 };
-
 export default App;
---
+++ src/MainRouter.tsx
@@ -0,0 +1,96 @@
+// MainRouter.tsx
+import {
+  IonSplitPane,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonMenuToggle,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from '@ionic/react';
+import { useLocation, Redirect, Route } from 'react-router-dom';
+import { documentOutline, mapOutline, informationOutline } from 'ionicons/icons';
+
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
+import Accueil from './pages/accueil/Accueil';
+import Login from './pages/login/Login';
+
+const MainRouter: React.FC = () => {
+  const location = useLocation();
+  const hideMenu = location.pathname === '/accueil' || location.pathname === '/login';
+
+  if (hideMenu) {
+    return (
+      <IonRouterOutlet>
+        <Route exact path="/accueil" component={Accueil} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/">
+          <Redirect to="/accueil" />
+        </Route>
+      </IonRouterOutlet>
+    );
+  }
+
+  return (
+    <IonSplitPane contentId="main-content">
+      <IonMenu contentId="main-content" type="overlay">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>TopoManager</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonList>
+            <IonMenuToggle autoHide={false}>
+              <IonItem routerLink="/tab1" routerDirection="none">
+                <IonIcon icon={documentOutline} slot="start" />
+                <IonLabel>Formulaire</IonLabel>
+              </IonItem>
+            </IonMenuToggle>
+            <IonMenuToggle autoHide={false}>
+              <IonItem routerLink="/tab2" routerDirection="none">
+                <IonIcon icon={mapOutline} slot="start" />
+                <IonLabel>Carte</IonLabel>
+              </IonItem>
+            </IonMenuToggle>
+            <IonMenuToggle autoHide={false}>
+              <IonItem routerLink="/tab3" routerDirection="none">
+                <IonIcon icon={informationOutline} slot="start" />
+                <IonLabel>Syncronisation</IonLabel>
+              </IonItem>
+            </IonMenuToggle>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+
+      <IonPage id="main-content">
+        <div
+          style={{
+            paddingTop: 'var(--ion-safe-area-top, 0)',
+            paddingBottom: 'var(--ion-safe-area-bottom, 0)',
+            height: '100%',
+          }}
+        >
+          <IonRouterOutlet>
+            <Route exact path="/tab1" component={Tab1} />
+            <Route exact path="/tab2" component={Tab2} />
+            <Route exact path="/tab3" component={Tab3} />
+            <Route exact path="/">
+              <Redirect to="/accueil" />
+            </Route>
+          </IonRouterOutlet>
+        </div>
+      </IonPage>
+    </IonSplitPane>
+  );
+};
+
+export default MainRouter;
