9399c1205b625433a06a81cea1cb0549b29203fb
Carbon-dev-code <andrianfandresenaa@gmail.com>
2025-07-21 09:59:24 +0300

    Modification
    Fandresena

--- Run.bat
+++ Run.bat
@@ -1,1 +1,5 @@
-rm -rf android && ionic build && npx cap add android && npx cap sync android && npx capacitor-assets generate --android && npx cap open android
+ionic build
+npx cap add android
+npx cap sync android
+npx capacitor-assets generate --android
+npx cap open android
